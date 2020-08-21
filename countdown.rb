3fb30d20110b1ca03f883c41c3ae21c3c9ae074d
elizabeth goodman <egoodman1180@icloud.com>
2020-08-21 14:32:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,7 +7,7 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 end
 
-countdown_with_sleep(number)
+def countdown_with_sleep(number)
   while number != 0
     number -= 1
     puts "#{number} SECOND(S)!"
